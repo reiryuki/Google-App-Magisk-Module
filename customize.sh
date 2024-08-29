68f1a225441ab9436b261ef8bd796877c668d36e
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2024-08-29 21:02:27 +0700

    customize.sh

--- customize.sh
+++ customize.sh
@@ -95,6 +95,35 @@ remove_sepolicy_rule
 ui_print " "
 
 # function
+conflict() {
+for NAME in $NAMES; do
+  DIR=/data/adb/modules_update/$NAME
+  if [ -f $DIR/uninstall.sh ]; then
+    sh $DIR/uninstall.sh
+  fi
+  rm -rf $DIR
+  DIR=/data/adb/modules/$NAME
+  rm -f $DIR/update
+  touch $DIR/remove
+  FILE=/data/adb/modules/$NAME/uninstall.sh
+  if [ -f $FILE ]; then
+    sh $FILE
+    rm -f $FILE
+  fi
+  rm -rf /metadata/magisk/$NAME\
+   /mnt/vendor/persist/magisk/$NAME\
+   /persist/magisk/$NAME\
+   /data/unencrypted/magisk/$NAME\
+   /cache/magisk/$NAME\
+   /cust/magisk/$NAME
+done
+}
+
+# conflict
+NAMES=FeaturesOfGoogleApp
+conflict
+
+# function
 cleanup() {
 if [ -f $DIR/uninstall.sh ]; then
   sh $DIR/uninstall.sh
